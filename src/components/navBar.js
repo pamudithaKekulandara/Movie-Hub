50e8781bc4164cd3735e4064a04131eb1e0e3df5
GeethOnion <geethmaka@gmail.com>
2022-05-19 21:09:56 +0530

    navbar changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -18,37 +18,45 @@ const NavBar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/items">
-                Movies
-              </a>
-            </li>
+          {!localStorage.getItem("admin") ? (
+            <>
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/items">
+                    Movies
+                  </a>
+                </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    About
+                  </a>
+                </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="/addmovie">
-                Admin
-              </a>
-            </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/addmovie">
+                    Admin
+                  </a>
+                </li>
 
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <></>
+          )}
+
           <div className="buttons">
             {localStorage.getItem("admin") ||
             localStorage.getItem("user") != null ? (
