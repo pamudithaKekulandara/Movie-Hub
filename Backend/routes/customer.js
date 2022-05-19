f8be0f4ece01a7cb5872ac27e5b4c1c42da72ec7
GeethOnion <geethmaka@gmail.com>
2022-05-19 13:54:57 +0530

    basic login and update movie function

--- Backend/routes/customer.js
+++ Backend/routes/customer.js
@@ -11,7 +11,6 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
 // This section will help you get a list of all the records.
 recordRoutes.route("/customer").get(function (req, res) {
   let db_connect = dbo.getDb("movieHub");
@@ -27,13 +26,11 @@ recordRoutes.route("/customer").get(function (req, res) {
 // This section will help you get a single record by id
 recordRoutes.route("/customer/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect
-      .collection("customer")
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);
-      });
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("customer").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
 });
 
 // This section will help you create a new record.
@@ -41,15 +38,13 @@ recordRoutes.route("/customer/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
     name: req.body.name,
-    phoneNo:req.body.phoneNo,
-    email:req.body.email,
-    password:req.body.password,
-    cardNo:req.body.cardNo,
-    cvc:req.body.cvc,
-    eDate:req.body.eDate,
-    eYear:req.body.eYear
-
-    
+    phoneNo: req.body.phoneNo,
+    email: req.body.email,
+    password: req.body.password,
+    cardNo: req.body.cardNo,
+    cvc: req.body.cvc,
+    eDate: req.body.eDate,
+    eYear: req.body.eYear,
   };
   db_connect.collection("customer").insertOne(myobj, function (err, res) {
     if (err) throw err;
@@ -59,31 +54,58 @@ recordRoutes.route("/customer/add").post(function (req, response) {
 
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();  
-  let myquery = { _id: ObjectId( req.params.id )};  
-  let newvalues = {    
-    $set: {      
-      name: req.body.name,
-      phoneNo:req.body.phoneNo,
-      email:req.body.email,
-      password:req.body.password,
-      cardNo:req.body.cardNo,
-      cvc:req.body.cvc,
-      eDate:req.body.eDate,
-      eYear:req.body.eYear
-    
-  },  
-};
-
-// This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      name: req.body.name,
+      phoneNo: req.body.phoneNo,
+      email: req.body.email,
+      password: req.body.password,
+      cardNo: req.body.cardNo,
+      cvc: req.body.cvc,
+      eDate: req.body.eDate,
+      eYear: req.body.eYear,
+    },
+  };
+
+  // This section will help you delete a record
+  recordRoutes.route("/:id").delete((req, response) => {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      response.json(obj);
+    });
   });
 });
-})
+
+recordRoutes.route("/customer/login").post((req, res) => {
+  let db_connect = dbo.getDb();
+  db_connect
+    .collection("customer")
+    .findOne({ email: req.body.email }, function (err, result) {
+      if (err) throw err;
+      if (result.password == req.body.password) {
+        res.json("granted");
+      } else {
+        res.json("denied");
+      }
+    });
+  // Employee.find(req.body.email)
+  //   .then((users) => {
+  //     for (user of users) {
+  //       // const decrypted = CryptoJS.AES.decrypt(
+  //       //   user.empPassword,
+  //       //   `${process.env.KEY}`
+  //       // ).toString(CryptoJS.enc.Utf8);
+  //       if (req.body.password == user.password) {
+  //         res.send("granted");
+  //       }
+  //     }
+  //     res.send("denied");
+  //   })
+  //   .catch((err) => res.status(400).json("Error: " + err));
+});
 module.exports = recordRoutes;
